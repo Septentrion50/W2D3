79f5d5ac1913badd33e3f34e633f8544db740efb
Septentrion50 <erwan56lecorre@gmail.com>
2021-04-13 22:38:53 +0200

    Ajout de la méthode wtf_pyramid à 01_pyramid.rb

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -29,4 +29,32 @@ def full_pyramid(num)
   end
 end
 
-full_pyramid(gets.chomp.to_i)
+def wtf_pyramid(num)
+  if num < 1
+    num = 1
+  elsif num > 25
+    num = 25
+  end
+  if num % 2 == 0
+    puts "Vous avez été envoyé bouler."
+  else
+    iter = 0
+    spaces = " "
+    stack = Array.new()
+    puts "Voici la pyramide :"
+    (num / 2 + 1).times do
+      first =  "#{spaces * (num - (iter + 1))}#{'#' * (iter + 1)}"
+      iter += 1
+      c = first.count("#") - 1
+      sec = "#" * c
+      puts first + sec
+      stack << first + sec
+    end
+    stack.pop
+    while stack.length != 0
+      puts stack.pop
+    end
+  end
+end
+
+wtf_pyramid(gets.chomp.to_i)

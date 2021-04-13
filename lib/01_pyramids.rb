b8d6921199dd95ab8a32066266fbfbb54a2337c0
Septentrion50 <erwan56lecorre@gmail.com>
2021-04-13 21:40:45 +0200

    ajout de la méthode full_pyramid à 01_pyramids.rb

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -12,4 +12,21 @@ def half_pyramid(num)
   end
 end 
 
-half_pyramid(gets.chomp.to_i)
+def full_pyramid(num)
+  if num < 1
+    num = 1
+  elsif num > 25
+    num = 25
+  end
+
+  spaces = " "
+  puts "Voici la pyramide :"
+  num.times do |iter|
+    first =  "#{spaces * (num - (iter + 1))}#{'#' * (iter + 1)}"
+    c = first.count("#") - 1
+    sec = "#" * c
+    puts first + sec
+  end
+end
+
+full_pyramid(gets.chomp.to_i)

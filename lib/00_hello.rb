8bb4d3f912f2048157c71df4aabbc8bf1c2aa12a
Septentrion50 <erwan56lecorre@gmail.com>
2021-04-13 21:34:18 +0200

    Nettoyage des fichiers inutiles

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,3 +1,11 @@
-def say_hello
-  puts "Hello!"
+def say_hello(first_name)
+  puts "Hello #{first_name}!"
 end
+
+def ask_first_name
+  puts "What's your name?"
+  first_name = gets.chomp
+  return first_name
+end
+
+say_hello(ask_first_name)
--- lib/01_hello.rb
+++
@@ -1,4 +0,0 @@
-def say_hello(first_name)
-  puts "Hello #{first_name}!"
-end
-
